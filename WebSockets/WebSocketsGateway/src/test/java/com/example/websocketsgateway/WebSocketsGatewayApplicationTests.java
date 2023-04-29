da6727f4838be9f9d483f303b1eaa176d791c0d5
ANdi g <95663488+DroptheKraken@users.noreply.github.com>
2023-04-30 00:10:19 +0200

    Websocket - initial setuo

---
+++ WebSockets/WebSocketsGateway/src/test/java/com/example/websocketsgateway/WebSocketsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
