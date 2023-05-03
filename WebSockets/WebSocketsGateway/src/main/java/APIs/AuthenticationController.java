d9da124884da03bf070b3b8587fe4074f13788be
Alex Toth <tothilian@gmail.com>
2023-05-03 06:46:44 +0200

    some grpc modifications and database connections

--- WebSockets/WebSocketsGateway/src/main/java/APIs/AuthenticationController.java
+++ WebSockets/WebSocketsGateway/src/main/java/APIs/AuthenticationController.java
@@ -2,6 +2,10 @@ package APIs;
 
 import DTO.LoginRequest;
 import DTO.TokenRequest;
+import DTO.User;
+import DTO.gRPCConverter;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import protos.UserObj;
+import protos.UserServiceGrpc;
 
 @RestController
 public class AuthenticationController
@@ -40,11 +46,25 @@ public class AuthenticationController
         }
     }
 
-    @GetMapping("/data")
-    public LoginRequest getData()
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signUp(@RequestBody User user)
     {
-        return new LoginRequest("blad","blad");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 5001)
+                .usePlaintext()
+                .build();
+
+        UserServiceGrpc.UserServiceBlockingStub stub
+                = UserServiceGrpc.newBlockingStub(channel);
+
+        UserObj userObj = gRPCConverter.getGrpcUserFromUser(user);
+
+        UserObj helloResponse2 = stub.addUser(userObj);
+        System.out.println(helloResponse2);
+
+        return new ResponseEntity <>(HttpStatus.OK);
     }
+
+
 }
 
 
