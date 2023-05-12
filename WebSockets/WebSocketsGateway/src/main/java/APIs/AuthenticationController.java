33e00bfc139d806b196d6ec8d2b6a7c32baa20f3
Arturs Silins <arturs.silins45@gmail.com>
2023-05-12 11:21:46 +0200

    SEP4-1 Created docker files and deploy pipeline

--- WebSockets/WebSocketsGateway/src/main/java/APIs/AuthenticationController.java
+++ WebSockets/WebSocketsGateway/src/main/java/APIs/AuthenticationController.java
@@ -17,54 +17,43 @@ import protos.UserObj;
 import protos.UserServiceGrpc;
 
 @RestController
-public class AuthenticationController
-{
+public class AuthenticationController {
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest)
-    {
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
         // Authenticate user and generate JWT token
         String token = JwtUtils.generateToken(loginRequest.getUsername(), loginRequest.getPassword());
 
         // Return token as response
         return new ResponseEntity<>(token, HttpStatus.OK);
 
-
     }
 
     @PostMapping("/validate")
-    public ResponseEntity<Void> validate(@RequestBody TokenRequest tokenRequest)
-    {
+    public ResponseEntity<Void> validate(@RequestBody TokenRequest tokenRequest) {
         boolean isValid = JwtUtils.validateToken(tokenRequest.getToken());
 
-        if (isValid)
-        {
+        if (isValid) {
             return new ResponseEntity<>(HttpStatus.OK);
-        } else
-        {
+        } else {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> signUp(@RequestBody User user)
-    {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 5001)
+    public ResponseEntity<Void> signUp(@RequestBody User user) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("172.22.0.2", 5001)
                 .usePlaintext()
                 .build();
 
-        UserServiceGrpc.UserServiceBlockingStub stub
-                = UserServiceGrpc.newBlockingStub(channel);
+        UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
 
         UserObj userObj = gRPCConverter.getGrpcUserFromUser(user);
 
         UserObj helloResponse2 = stub.addUser(userObj);
         System.out.println(helloResponse2);
 
-        return new ResponseEntity <>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
 }
-
-
