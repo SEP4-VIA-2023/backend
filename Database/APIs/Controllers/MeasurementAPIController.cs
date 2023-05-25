ec3ff7f3e95143a72ac8fba93724baca9137fe87
Alex Toth <tothilian@gmail.com>
2023-05-25 14:02:47 +0200

    moved websockets into api project
    made a single runnable project for the websockets and api

---
+++ Database/APIs/Controllers/MeasurementAPIController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIs.Controllers;
+
+public class MeasurementAPIController : ControllerBase
+{
+    
+}
--- Database/Logic/Logic/UserLogic.cs
+++ Database/Logic/Logic/UserLogic.cs
@@ -1,39 +1,39 @@
-using Logic.Interface;
-using Logic.Security;
-using Model;
-
-namespace Logic.Logic;
-
-public class UserLogic : IUserLogic
-{
-    private readonly Tokens _tokens;
-
-    public UserLogic(Tokens tokens)
-    {
-        _tokens = tokens;
-    }
-    public UserDTO CreateUser(UserDTO dto)
-    {
-        PasswordHashing.HashPassword(dto.Password!, out byte[] passwordHash, out byte[] salt);
-
-        throw new NotImplementedException();
-    }
-
-    public string LoginUser(UserDTO dto)
-    {
-        // Retrieve user by username from DB
-
-        // Not implemented
-
-        User reply = new();
-
-        // Verify password
-
-        if (!PasswordHashing.VerifyPasswordHash(dto.Password!, reply.Password!, reply.Salt!))
-        {
-            throw new Exception("Incorrect password");
-        }
-
-        return _tokens.CreateToken(reply);
-    }
-}
+// using Logic.Interface;
+// using Logic.Security;
+// using Model;
+//
+// namespace Logic.Logic;
+//
+// public class UserLogic : IUserLogic
+// {
+//     private readonly Tokens _tokens;
+//
+//     public UserLogic(Tokens tokens)
+//     {
+//         _tokens = tokens;
+//     }
+//     public UserDTO CreateUser(UserDTO dto)
+//     {
+//         PasswordHashing.HashPassword(dto.Password!, out byte[] passwordHash, out byte[] salt);
+//
+//         throw new NotImplementedException();
+//     }
+//
+//     // public string LoginUser(UserDTO dto)
+//     // {
+//     //     // Retrieve user by username from DB
+//     //
+//     //     // Not implemented
+//     //
+//     //     User reply = new User();
+//     //
+//     //     // Verify password
+//     //
+//     //     if (!PasswordHashing.VerifyPasswordHash(dto.Password!, reply.Password!, reply.Salt!))
+//     //     {
+//     //         throw new Exception("Incorrect password");
+//     //     }
+//     //
+//     //     return _tokens.CreateToken(reply);
+//     // }
+// }
