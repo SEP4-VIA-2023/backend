895619bcb6c6c4b2637d890109f7b3f221815598
Alex Toth <tothilian@gmail.com>
2023-05-27 13:25:42 +0200

    cleaned up the code
    Cleaned the code and added a missing package in dotnet

--- Database/EFCDataAccess/DAOs/PresetDAO.cs
+++ Database/EFCDataAccess/DAOs/PresetDAO.cs
@@ -36,12 +36,32 @@ public class PresetDAO: IPresetDAO
         return await _dataContext.Presets.ToListAsync();
     }
 
-    public async Task<Preset> UpdateAsync(Preset preset)
+    public async Task<Preset> UpdateAsync(int presetId, Preset updatedPreset)
     {
-        _dataContext.Presets.Update(preset);
+        var preset = await _dataContext.Presets.FindAsync(presetId);
+    
+        if (preset == null)
+        {
+            throw new ArgumentException("Preset not found");
+        }
+    
+        // Update the preset properties with the new values
+        preset.Name = updatedPreset.Name;
+        preset.MinHumidity = updatedPreset.MinHumidity;
+        preset.MaxHumidity = updatedPreset.MaxHumidity;
+        preset.MinCo2 = updatedPreset.MinCo2;
+        preset.MaxCo2 = updatedPreset.MaxCo2;
+        preset.MinTemperature = updatedPreset.MinTemperature;
+        preset.MaxTemperature = updatedPreset.MaxTemperature;
+        preset.Servo = updatedPreset.Servo;
+        preset.DeviceId = updatedPreset.DeviceId;
+        // ... update other properties as needed
+    
         await _dataContext.SaveChangesAsync();
+    
         return preset;
     }
+
 
     public async Task<Preset> DeleteAsync(int id)
     {
